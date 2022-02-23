6b2fa07420d48d0f33948c4a7f66e476d4f070fc
Yun Gu <guy36@cardiff.ac.uk>
2022-02-23 14:16:46 +0000

    frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [country, setCountry] = useState('')
+  const [position, setPosition] = useState('')
+  const [wage, setWage] = useState(0)
+
+  const displayInfo = ()=>{
+    console.log(name + age + country + position + wage);
+  }
+
   return (
-    <div className="App">
-    Hello
+    <div className="information">
+    <label>Name:</label>
+    <input type="text" onChange={(event)=>{setName(event.target.value)}}></input>
+    <label>Age:</label>
+    <input type="number" onChange={(event)=>{setAge(event.target.value)}}></input>
+    <label>Country:</label>
+    <input type="text" onChange={(event)=>{setCountry(event.target.value)}}></input>
+    <label>Position:</label>
+    <input type="text" onChange={(event)=>{setPosition(event.target.value)}}></input>
+    <label>Wage(year):</label>
+    <input type="number" onChange={(event)=>{setWage(event.target.value)}}></input>
+    <button type="button" onClick={displayInfo}>Add Employee</button>
     </div>
   );
 }
